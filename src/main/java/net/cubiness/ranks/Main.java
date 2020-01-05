902e21914c2d89f247e442fb1b6e777e014d6e95
Neil Macneale <neil.macneale.v@gmail.com>
2020-01-04 21:51:36 -0800

    Started using ~/.aws/credentials

--- src/main/java/net/cubiness/ranks/Main.java
+++ src/main/java/net/cubiness/ranks/Main.java
@@ -2,10 +2,12 @@ package net.cubiness.ranks;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -13,30 +15,19 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
-  private String access_key;
-  private String secret_key;
   private Table ranks;
 
   @Override
   public void onEnable() {
     getServer().getPluginManager().registerEvents(this, this);
-    access_key = getConfig().getString("aws.key.access");
-    secret_key = getConfig().getString("aws.key.secret");
-    if (access_key == null || secret_key == null) {
-      access_key = "ACCESSKEY";
-      secret_key = "SECRETKEY";
-    }
-    if (access_key.equals("ACCESSKEY") || secret_key.equals("SECRETKEY")) {
-      getLogger().warning("Invalid config.yml! Please replace aws.key.access and aws.key.secret with your aws keys.");
-    } else {
-      BasicAWSCredentials awsCreds = new BasicAWSCredentials(access_key, secret_key);
 
-      AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-              .withCredentials((new AWSStaticCredentialsProvider(awsCreds)))
-              .build();
-      DynamoDB dynamoDB = new DynamoDB(client);
-      ranks = dynamoDB.getTable("minecraft");
-    }
+    AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+            .withRegion(Regions.US_WEST_2)
+            .build();
+    getLogger().info("Client: " + client);
+    DynamoDB dynamoDB = new DynamoDB(client);
+    getLogger().info("Db: " + dynamoDB);
+    ranks = dynamoDB.getTable("cubiness-ranks");
     getLogger().info("Ranks plugin loaded!");
   }
 
@@ -49,7 +40,6 @@ public class Main extends JavaPlugin implements Listener {
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (label.equals("rank")) {
       // Item item = ranks.getItem("free", 101);
-      sender.sendMessage(access_key);
       sender.sendMessage(ranks + "");
       return true;
     }
