88892d4f83c25b2c56a5b8bcaf26139c9cf94e2d
Neil Macneale <neil.macneale.v@gmail.com>
2020-01-05 14:54:04 -0800

    Added basic config.yml loading for table name and profile name

--- src/main/java/net/cubiness/ranks/Main.java
+++ src/main/java/net/cubiness/ranks/Main.java
@@ -18,11 +18,25 @@ public class Main extends JavaPlugin implements Listener {
   public void onEnable() {
     getServer().getPluginManager().registerEvents(this, this);
 
+    String profileName = (String) getConfig().get("aws.profile");
+    String tableName = (String) getConfig().get("aws.table");
+
+    if (profileName == null) {
+      getLogger().info("Please set aws.profile in config.yml to a valid config in ~/.aws/credentials");
+      getServer().getPluginManager().disablePlugin(this);
+      return;
+    }
+    if (tableName == null) {
+      getLogger().info("Please set aws.table in config.yml to a valid table in aws");
+      getServer().getPluginManager().disablePlugin(this);
+      return;
+    }
+
     AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-            .withCredentials(new ProfileCredentialsProvider("ranks-table"))
+            .withCredentials(new ProfileCredentialsProvider(profileName))
             .build();
     DynamoDB dynamoDB = new DynamoDB(client);
-    ranks = dynamoDB.getTable("cubiness-ranks");
+    ranks = dynamoDB.getTable(tableName);
     getLogger().info("Ranks plugin loaded!");
   }
 
